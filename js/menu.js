0bfd9e6d0ed59f7b3aa5f7f0aa4948052465064a
jorgedvx <jorgecongalopez02@gmail.com>
2024-12-04 12:30:49 -0500

    second commit

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 const openMenu = document.querySelector("#open-menu");
 const closeMenu = document.querySelector("#close-menu");
 const aside = document.querySelector("aside");
-const botonesCategorias = document.querySelectorAll(".boton-categoria");
+
 
 openMenu.addEventListener("click", () => {
     aside.classList.add("aside-visible");
